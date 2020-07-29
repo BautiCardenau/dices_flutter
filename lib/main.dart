3268a23ea4d89a90b7c493d85156004078ac0be7
BautiCardenau <bcardenau@itba.edu.ar>
2020-07-29 10:03:41 +0200

    stateless into stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,9 +13,17 @@ void main() {
   );
 }
 
-class Dices extends StatelessWidget {
+class Dices extends StatefulWidget {
+  @override
+  _DicesState createState() => _DicesState();
+}
+
+class _DicesState extends State<Dices> {
+  var upperDiceNr = 1;
+  var lowerDiceNr = 1;
   @override
   Widget build(BuildContext context) {
+    Random random = new Random();
     return SafeArea(
       child: Container(
         child: Center(
@@ -26,12 +36,26 @@ class Dices extends StatelessWidget {
               Expanded(
                   child: Padding(
                 padding: const EdgeInsets.all(20.0),
-                child: Image.asset('images/dice1.png'),
+                child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        upperDiceNr = random.nextInt(5) + 1;
+                        print(upperDiceNr);
+                      });
+                    },
+                    child: Image.asset('images/dice$upperDiceNr.png')),
               )),
               Expanded(
                   child: Padding(
                 padding: const EdgeInsets.all(20.0),
-                child: Image.asset('images/dice2.png'),
+                child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        lowerDiceNr = random.nextInt(5) + 1;
+                        print(lowerDiceNr);
+                      });
+                    },
+                    child: Image.asset('images/dice$lowerDiceNr.png')),
               )),
               SizedBox(
                 height: 50,
